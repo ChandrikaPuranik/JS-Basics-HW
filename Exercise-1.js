7c7c43ac4adfb0ee1fb5526608c2d6d212081cc9
chandrika <chandpuru@gmail.com>
2023-01-07 08:00:03 -0500

    changes

--- Exercise-1.js
+++ Exercise-1.js
@@ -11,22 +11,3 @@ function findbiggest(a, b) {
     }
     console.log(findbiggest(100, 200))
 
-//3 write a function that returns boolean flag if a number is between 5000 and 9999
-
-function isBoolean(num)
-    {
-    if (num>5000 && num<9999)
-    return true;
-    else
-    return false;
-    }
-    console.log(isBoolean(10000))
-
-    function isBoolean(num)
-    {
-    if (num>5000 && num<9999)
-    return true;
-    else
-    return false;
-    }
-    console.log(isBoolean(6000))
--- Exercise-3.js
+++ Exercise-3.js
@@ -17,3 +17,11 @@ function isBoolean(num)
     return false;
     }
     console.log(isBoolean(6000))
+
+    //4 write a function that accepts a number and returns the cube root of the number
+           
+    function cuberoot(x)
+    {
+    return Math.cbrt(x, 1/3)
+    }
+    console.log(cuberoot(55))
