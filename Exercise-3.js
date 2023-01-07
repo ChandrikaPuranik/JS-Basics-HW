284572d87fb935b4370fbabef700c92926e58465
chandrika <chandpuru@gmail.com>
2023-01-07 07:54:42 -0500

    Exer 3

--- Exercise-3.js
+++ Exercise-3.js
@@ -1,16 +1,3 @@
-//1 write a function that counts the number of character in a string the method shoud return thenumber
-
-var chand = 5
-console.log(chand)
-
-//2 write a function that accepts an two numbers and return the largest number
-
-function findbiggest(a, b) {
-    if (a > b) return a
-    else return b
-    }
-    console.log(findbiggest(100, 200))
-
 //3 write a function that returns boolean flag if a number is between 5000 and 9999
 
 function isBoolean(num)
