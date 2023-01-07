d616d22630e7e85277ad4fb67f9b7ce75a3e5069
chandrika <chandpuru@gmail.com>
2023-01-07 08:27:05 -0500

    Exer2

---
+++ Exercise-2.js
@@ -0,0 +1,7 @@
+//2 write a function that accepts an two numbers and return the largest number
+
+function findbiggest(a, b) {
+    if (a > b) return a
+    else return b
+    }
+    console.log(findbiggest(100, 200))
